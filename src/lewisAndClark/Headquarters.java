53295ac82489e6ae15b2c3b19427f2694ff5f9aa
Saahith Janapati <itzsaahith01@gmail.com>
2023-01-11 15:38:30 -0500

    begin work on lewisAndClark (pathfinding

---
+++ src/lewisAndClark/Headquarters.java
@@ -0,0 +1,39 @@
+package lewisAndClark;
+
+import battlecode.common.*;
+
+public class Headquarters extends Robot {
+
+    public Headquarters(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void run() throws GameActionException{
+        super.run();
+//        System.out.println("running");
+        Direction dir = directions[rng.nextInt(directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+        if (rc.canBuildAnchor(Anchor.STANDARD)) {
+//            System.out.println("building an anchor");
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
+        }
+        if (rng.nextBoolean()) {
+//            System.out.println("Trying to build a carrier");
+
+            // Let's try to build a carrier.
+            rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+//            System.out.println("Trying to build a lanuch");
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+    }
+}
