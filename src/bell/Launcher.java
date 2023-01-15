357d6d10941fd160bd800d74c351f14813086d90
Srikar <srikarg89@gmail.com>
2023-01-15 12:21:54 -0500

    Smarter launcher micro

--- src/bell/Launcher.java
+++ src/bell/Launcher.java
@@ -92,14 +92,62 @@ public class Launcher extends Robot {
             return;
         }
 
-        if(haveUncommedIsland()){
-            returnToClosestHQ();
+        // If you're in a fight w/ an enemy, run launcher micro
+        RobotInfo[] enemies = rc.senseNearbyRobots(myType.visionRadiusSquared, opponent);
+        boolean dangerNearby = false;
+        for(RobotInfo info : enemies){
+            if(info.type == RobotType.LAUNCHER || info.type == RobotType.CARRIER){
+                dangerNearby = true;
+            }
         }
-        else if(isAttacking){
-            runAttackMovement();
-        }
-        else{
-            runDefensiveMovement();
+        if(dangerNearby){
+            RobotInfo closestDanger = null;
+            for(RobotInfo info : enemies){
+                if(info.type == RobotType.LAUNCHER || info.type == RobotType.CARRIER){
+                    if(closestDanger == null || myLoc.distanceSquaredTo(info.location) < myLoc.distanceSquaredTo(closestDanger.location)){
+                        closestDanger = info;
+                    }
+                }
+            }
+
+            // If you're within range, try attacking then move out.
+            runAttack();
+            // If you're outside of range but your attack is ready, move in then attack, but only move in if attack is ready
+            if(rc.isActionReady()){
+                Direction enemyDir = myLoc.directionTo(closestDanger.location);
+                // If I can move towards enemy and attack him, then do it.
+                Direction[] potentialMoveDirs = {enemyDir, enemyDir.rotateLeft(), enemyDir.rotateRight()};
+                for(Direction potentialMoveDir : potentialMoveDirs){
+                    if(!rc.canMove(potentialMoveDir)){
+                        continue;
+                    }
+                    // TODO: Check for currents and cooldowns and stuff
+                    // TODO: Do the whole "do we have more allied troops than enemy troops" thing to figure out if it's a fight worth taking
+                    // Can prolly copy some of that stuff from last year.
+                    // If I can move in and attack, then do that.
+                    if(myLoc.add(potentialMoveDir).distanceSquaredTo(closestDanger.location) <= myType.actionRadiusSquared){
+                        rc.move(potentialMoveDir);
+                        runAttack();
+                    }
+                }
+            }
+            // If you couldn't move in then attack, then simply move away until your attack comes back.
+            if(rc.isMovementReady()){
+                Direction enemyDir = myLoc.directionTo(closestDanger.location);
+                MapLocation farAway = myLoc.subtract(enemyDir).subtract(enemyDir).subtract(enemyDir).subtract(enemyDir);
+                nav.goToFuzzy(farAway, 0);
+            }
+        } else{
+            // Otherwise, run normal launcher movement
+            if(haveUncommedIsland()) {
+                returnToClosestHQ();
+            }
+            else if(isAttacking){
+                runAttackMovement();
+            }
+            else{
+                runDefensiveMovement();
+            }
         }
     }
 
